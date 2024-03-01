2400b16c8f76efb569c3292d233224bcbdd74bb9
Nadav <nadav.borenstein@mail.huji.ac.il>
2024-03-01 15:50:11 +0100

    prettier code

--- README.md
+++ README.md
@@ -4,23 +4,47 @@ Repository of the paper ["Investigating Human Values in Online Communities"](htt
 
 Still a work in progress.
 
-1. Subreddit Schwartz values can be found in `outputs/subreddit_schwartz_values.csv`.
-2. Figures from the paper can be found in `outputs/`
+1. The list of subreddit we analised can be found in `outputs/subreddits.txt`.
+2. Subreddit Schwartz values can be found in `outputs/subreddit_schwartz_values.csv`.
+3. Figures from the paper can be found in `outputs/`.
 
 ## Usage
 To use the code, clone it to a new folder, cd into it, and run `conda env create -f environment.yml`, which will create a fresh conda enviroment with all the needed dependencies.
 
 ### Data Collection
-First, download the posts data from `https://pushshift.io/` (the `RS_*.zst` files). In the paper, we analised the files `RS_2022-01.zst, ..., RS_2022-09.zst`, but any set of `RS_*.zst` files will do. The files are very large (compresed size can surpass 15GB, and the uncompressed size can surpass 200GB), so save them in a system with enough storage.
+First, download the posts data from [pushshift.io](https://pushshift.io/) (the `RS_*.zst` files). In the paper, we analised the files `RS_2022-01.zst, ..., RS_2022-09.zst`, but any set of `RS_*.zst` files will do. These files are very large (compresed size can surpass 15GB, and the uncompressed size can surpass 200GB), so save them in a system with enough storage.
 
+Next, run the script 
 
+```bash
+python collect_reddit_data_to_label.py [PATH TO PUSHIFT] [SAVE PATH]
+```
+
+It will collect up to 1000 random posts from each subreddit in `outputs/subreddits.txt` and store them in `[SAVE PATH]`.
 
 ### Training
 
-We use the training code from `https://github.com/m0re4u/value-disagreement`, the repositopry of the paper "Do Differences in Values Influence Disagreements in Online Discussions?".
-Either use their repository to train the model, or follow their instructions while using ours, which contains a simplified, slim, version of their code. The training script is `scripts/training_moral_values/train_paper.py`.
+We use the training code from `https://github.com/m0re4u/value-disagreement`, the repositopry of the paper "*Do Differences in Values Influence Disagreements in Online Discussions?*".
+Either use their repository to train the model, or follow their instructions while using ours, which contains a simplified, slim, version of their code. The training script is `scripts/training_moral_values/train_paper.py`. To use it, run
+
+```bash
+python scripts/training_moral_values/train_paper.py both --use_model microsoft/deberta-v3-base --n_runs 1
+```
+
+Do not forget to download `ValueEval` and `ValueNet` datasets and store them in `data` directory.
+
+### Labeling subreddits
+
+To label the posts collected by `collect_reddit_data_to_label.py` with Schwartz values, run
+
+```bash
+python label_subreddits.py [POSTS PATH] [MODEL CHECKPOINT] [SAVE DIR]
+```
+
+Where `[POSTS PATH]` is the same as `[SAVE PATH]` from the data collection step, `[MODEL CHECKPOINT]` is the checkpoint of the trained model from the training step. The code also supports basic parrallelism -- you can include the arguments `--subset [SUBSET ID]` and `--total [N SUBSETS]` to process only subset number `[SUBSET ID]` out of `[N SUBSETS]`.
+
 
 ### Data analysis
 
-TBD
+TBD. The notebook `analyse_subreddits.ipynb` contains some reference code.
 
--- label_subreddits.py
+++ label_subreddits.py
@@ -1,53 +1,75 @@
-import pickle
-import torch
-from value_disagreement.extraction import ValueNetExtractor
-from transformers import AutoTokenizer
-from collections import defaultdict
-import pandas as pd
-from lingua import Language, LanguageDetectorBuilder
 import argparse
-import spacy
+import pickle
+from collections import defaultdict
+from typing import List, Dict
 
+import pandas as pd
+import spacy
+import torch
+from lingua import Language, LanguageDetectorBuilder
+from transformers import AutoTokenizer
+from value_disagreement.extraction import ValueNetExtractor
+
+# Load English language model
 nlp = spacy.load("en_core_web_sm")
 
-ROOT_DIR = "[ROOT_DIR]"
-model_checkpoint = f"{ROOT_DIR}/value_extractor_model"
-tokenizer_override = "microsoft/deberta-v3-base"
-SUBREDDITS_PATH = "[SUBREDDITS_PATH]"
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--subset", type=int, required=False, default=0)
-    parser.add_argument("--total", type=int, required=False, default=1)
-    parser.add_argument("--batch_size", type=int, required=False, default=8)
-    parser.add_argument("--save_path", type=str, required=True)
-    parser.add_argument("--broken_subs", action="store_true")
+tokenizer_override = "microsoft/deberta-v3-base"
+
+
+def parse_args() -> argparse.Namespace:
+    """
+    Parse command-line arguments.
+    """
+    parser = argparse.ArgumentParser(description="Label subreddits")
+    parser.add_argument("posts_path", type=str, help="Path to the subreddit posts")
+    parser.add_argument(
+        "model_checkpoint", type=str, help="Path to the model checkpoint"
+    )
+    parser.add_argument("save_dir", type=str, help="Path to save the data")
+    parser.add_argument("--subset", type=int, default=0)
+    parser.add_argument("--total", type=int, default=1)
+    parser.add_argument("--batch_size", type=int, default=8)
     return parser.parse_args()
 
 
-def filter_non_english(comments, detector):
-    filtered_comments = [
-        c for c in comments if detector.detect_language_of(c) == Language.ENGLISH
-    ]
-    return filtered_comments
+def filter_non_english(
+    comments: List[str], detector: LanguageDetectorBuilder
+) -> List[str]:
+    """
+    Filter out non-English comments.
+    """
+    return [c for c in comments if detector.detect_language_of(c) == Language.ENGLISH]
 
 
-def get_subreddit_posts_random_sample(subreddit, posts=True, is_tuple=True):
+def get_subreddit_posts_random_sample(
+    subreddit: List[str], posts=True, is_tuple=True
+) -> List[str]:
+    """
+    Get a random sample of subreddit posts.
+    """
     if posts:
-        if not is_tuple:
-            texts = [post["selftext"] + post["title"] for post in subreddit]
-        else:
-            texts = [post[1] + post[2] for post in subreddit]
+        texts = (
+            [post[1] + post[2] for post in subreddit]
+            if is_tuple
+            else [post["selftext"] + post["title"] for post in subreddit]
+        )
     else:
-        if not is_tuple:
-            texts = [comment["body"] for comment in subreddit]
-        else:
-            texts = [comment[1] for comment in subreddit]
-    texts = [c for c in texts if c is not None and len(c) > 0]
-    return texts
+        texts = (
+            [comment[1] for comment in subreddit]
+            if is_tuple
+            else [comment["body"] for comment in subreddit]
+        )
+
+    return [c for c in texts if c is not None and len(c) > 0]
 
 
-def probs_to_dict_of_probs(ve, comments, probs):
+def probs_to_dict_of_probs(
+    ve: ValueNetExtractor, comments: List[str], probs: torch.Tensor
+) -> Dict[str, float]:
+    """
+    Convert probabilities to a dictionary of probabilities.
+    """
     probs_dict = defaultdict(float)
     for i in range(len(comments)):
         comment_probs = probs[i * len(ve.values) : (i + 1) * len(ve.values)]
@@ -56,28 +78,29 @@ def probs_to_dict_of_probs(ve, comments, probs):
     probs_dict = {
         k: v / len(comments) for k, v in probs_dict.items()
     }  # average over comments
-    probs_dict = {k: v.item() for k, v in probs_dict.items()}
-    return probs_dict
+    return {k: v.item() for k, v in probs_dict.items()}
 
 
-def get_probabilities(ve, comments):
+def get_probabilities(ve: ValueNetExtractor, comments: List[str]) -> torch.Tensor:
+    """
+    Get probabilities for each comment.
+    """
     user_dataset = ve.create_user_dataset(comments, 0)
     user_dataset = user_dataset.map(ve.tokenize, batched=True)
     predictions = ve.trainer.predict(user_dataset).predictions
     probabilities = torch.softmax(torch.tensor(predictions), dim=1)
-    probabilities = probabilities[:, 1]
-    return probabilities
+    return probabilities[:, 1]
 
 
 def main(args):
-    ve = ValueNetExtractor(model_checkpoint, batch_size=args.batch_size)
+    ve = ValueNetExtractor(args.model_checkpoint, batch_size=args.batch_size)
     ve.tokenizer = AutoTokenizer.from_pretrained(
         tokenizer_override
-    )  # we need to override the tokenizer because their code is weird
+    )  
     detector = LanguageDetectorBuilder.from_all_languages().build()
     print("loaded model")
 
-    data = pickle.load(open(SUBREDDITS_PATH, "rb"))
+    data = pickle.load(open(args.posts_path, "rb"))
     metadata = {k: {} for k in data.keys()}
     print("loaded data")
 
@@ -87,38 +110,38 @@ def main(args):
     subreddits.sort()
     subreddits = subreddits[args.subset :: args.total]
 
+    save_path = args.save_dir if args.total == 1 else f"{args.save_dir}_{args.subset}"
+    
     for i, subreddit in enumerate(subreddits):
-        if len(data[subreddit]) < 100:
-            continue
         try:
-            comments = get_subreddit_posts_random_sample(data[subreddit])
+            posts = get_subreddit_posts_random_sample(data[subreddit])
         except:
             continue
-        english_comments = filter_non_english(comments, detector)
-        if len(english_comments) < 250:
+        english_posts = filter_non_english(posts, detector)
+        if len(english_posts) < 250:
             continue
         print(subreddit)
-        print(len(english_comments))
+        print(len(english_posts))
         try:
-            probs = get_probabilities(ve, english_comments)
+            probs = get_probabilities(ve, english_posts)
         except RuntimeError:
             print("RuntimeError: ", subreddit)
             failed.add(subreddit)
             continue
-        probs_dict = probs_to_dict_of_probs(ve, english_comments, probs)
+        probs_dict = probs_to_dict_of_probs(ve, english_posts, probs)
         probs_dict.update(metadata[subreddit])
-        probs_dict["num_total_comments"] = len(comments)
-        probs_dict["num_english_comments"] = len(english_comments)
+        probs_dict["num_total_posts"] = len(posts)
+        probs_dict["num_english_posts"] = len(english_posts)
         subreddit_values[subreddit] = probs_dict
         print(probs_dict)
         if i % 100 == 0:
             df = pd.DataFrame(subreddit_values).T
-            df.to_csv(f"{args.save_path}_{args.subset}.csv")
+            df.to_csv(f"{save_path}/subreddit_schwarts_values.csv")
 
     df = pd.DataFrame(subreddit_values).T
-    df.to_csv(f"{args.save_path}_{args.subset}.csv")
+    df.to_csv(f"{save_path}/subreddit_schwarts_values.csv")
     print(failed)
-    pickle.dump(failed, open(f"{args.save_path}_{args.subset}_failed.pkl", "wb"))
+    pickle.dump(failed, open(f"{save_path}/subreddit_schwarts_value_failed.pkl", "wb"))
 
 
 if __name__ == "__main__":
